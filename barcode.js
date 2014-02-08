fb8ff9951ab90e30c0f3b02a0b7ab4dc86a15f41
kristenmills <kristen@kristen-mills.com>
2014-02-08 09:07:05 -0500

    Commented things

--- barcode.js
+++ barcode.js
@@ -1,6 +1,7 @@
 var mycroft = require('mycroft');
 var client = mycroft.Mycroft('barcode', './app.json', 'localhost', 1847);
 
+// Handler for APP_DEPENDENCY
 client.on('APP_DEPENDENCY', function(data){
   client.up();
   promptMessage();
@@ -9,6 +10,7 @@ client.on('APP_DEPENDENCY', function(data){
 client.connect();
 client.sendManifest();
 
+//Prompts for a barcode
 function promptMessage() {
   prompt.start();
   prompt.get(['barcode'], function(err, result) {
